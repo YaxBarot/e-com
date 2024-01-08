5e63e4f27c16cfc5ec472506a543173132d4c209
YaxBarot <yaxbarot2602@gmail.com>
2024-01-08 18:40:30 -0500

    database setup

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = "e_commerce.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sql5675544',
+        'USER': 'sql5675544',
+        'PASSWORD': 'TFfbKJKCT2',
+        'HOST': 'sql5.freemysqlhosting.net',
+        'PORT': '3306',
+        'OPTION': {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4"
+        },
     }
 }
 
